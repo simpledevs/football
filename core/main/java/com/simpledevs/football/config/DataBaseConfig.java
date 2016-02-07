6e4fce3d277e46dfd120eb43e10b46546b1f77d6
Gagik <445501@gmail.com>
2016-02-08 00:16:43 +0400

    change db to posgresql

--- core/main/java/com/simpledevs/football/config/DataBaseConfig.java
+++ core/main/java/com/simpledevs/football/config/DataBaseConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
@@ -44,7 +46,21 @@ public class DataBaseConfig {
         dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
         dataSource.setUsername(env.getRequiredProperty("db.username"));
         dataSource.setPassword(env.getRequiredProperty("db.password"));
+        dataSource.setInitialSize(Integer.parseInt(env.getRequiredProperty("db.initialSize")));
+        dataSource.setMinIdle(Integer.parseInt(env.getRequiredProperty("db.minIdle")));
+        dataSource.setMaxIdle(Integer.parseInt(env.getRequiredProperty("db.maxIdle")));
+        dataSource.setTimeBetweenEvictionRunsMillis(Long.parseLong(env.getRequiredProperty("db.timeBetweenEvictionRunsMillis")));
+        dataSource.setMinEvictableIdleTimeMillis(Long.parseLong(env.getRequiredProperty("db.minEvicableIdleTimeMillis")));
+        dataSource.setTestOnBorrow(Boolean.parseBoolean(env.getRequiredProperty("db.testOnBorrow")));
+        dataSource.setValidationQuery(env.getRequiredProperty("db.validationQuery"));
         return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager() {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return manager;
     }
 
     public Properties getHibernateProperties() {
--- core/main/java/com/simpledevs/football/entity/League.java
+++ core/main/java/com/simpledevs/football/entity/League.java
@@ -1,7 +1,27 @@
 package com.simpledevs.football.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class League {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private long id;
+
+    public League() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
